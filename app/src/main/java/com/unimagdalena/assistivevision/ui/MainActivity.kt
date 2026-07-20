c057bce72388826c2649439240a448d1ed2aa74f
Diomedes Cerda <kenmatwofour@gmail.com>
2026-07-20 15:40:00 -0500

    Adopt View Binding and edge-to-edge support in MainActivity

--- app/src/main/java/com/unimagdalena/assistivevision/ui/MainActivity.kt
+++ app/src/main/java/com/unimagdalena/assistivevision/ui/MainActivity.kt
@@ -6,21 +6,31 @@ import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.camera.view.PreviewView
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import com.unimagdalena.assistivevision.R
+import com.unimagdalena.assistivevision.databinding.ActivityMainBinding
 import com.unimagdalena.assistivevision.modules.CaptureModule
 import com.unimagdalena.assistivevision.viewmodel.MainViewModel
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var binding: ActivityMainBinding
     private lateinit var captureModule: CaptureModule
     private val viewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
 
         if(allPermissionsGranted()) {
             startCamera()
@@ -34,11 +44,10 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun startCamera() {
-        val previewView = findViewById<PreviewView>(R.id.previewView)
         captureModule = CaptureModule(this, this) { bitmap ->
             viewModel.processFrame(bitmap)
         }
-        captureModule.start(previewView)
+        captureModule.start(binding.previewView)
     }
 
     override fun onRequestPermissionsResult(
